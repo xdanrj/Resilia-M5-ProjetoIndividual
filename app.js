f8b0e86b29c4d650a90cbf6fff666f757578366d
xdanrj <xdanrj@gmail.com>
2023-03-25 21:21:31 -0300

    Loop, cor e variáveis aprim., script adic.

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 import chalk from 'chalk'
 import promptSync from 'prompt-sync';
-
 const prompt = promptSync();
 
-var listaEntradas = []
-var loopAtivo = true
+let listaEntradas = []
 
-while(loopAtivo === true){
-    var entrada = prompt(`Insira uma propriedade CSS (digite "SAIR" para sair): `)
-    
-    if ( !(entrada == "sair" || (entrada == "SAIR")) ) {
+function corAleatoria(item) {
+    const cor = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'gray', 'redBright', 'greenBright', 'yellowBright', 'blueBright', 'magentaBright', 'cyanBright']
+    let numeroRandom = Math.floor(Math.random() * 12)
+    return chalk[cor[numeroRandom]](item)
+}
+
+while (true) {
+    let entrada = prompt(corAleatoria(`Insira uma propriedade CSS (digite "SAIR" para sair): `))
+    if (entrada.localeCompare("sair", undefined, { sensitivity: 'accent' }) != 0) {
         listaEntradas.push(entrada)
     }
-    else{
+    else {
         listaEntradas.sort()
         break
     }
 }
-
-console.log("\nLista das propriedades em ordem alfabética: ")
-
-var cor = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'gray', 'redBright', 'greenBright', 'yellowBright', 'blueBright', 'magentaBright', 'cyanBright']
-
-for(var i=0; i < listaEntradas.length; i++){
-        var numeroRandom = Math.floor(Math.random() * 12)
-        console.log(chalk[cor[numeroRandom]](listaEntradas[i]))
+console.log(corAleatoria("\nLista das propriedades em ordem alfabética: "))
+if (listaEntradas.length === 0) {
+    console.log(corAleatoria("Você não digitou nenhuma propriedade."))
+}
+else {
+    for (let i = 0; i < listaEntradas.length; i++) {
+        console.log(corAleatoria(listaEntradas[i]))
     }
+}
